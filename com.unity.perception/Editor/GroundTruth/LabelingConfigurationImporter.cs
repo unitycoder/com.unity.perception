b8a719f8dfc00cad2af60f83d77691e74d562b2c
Jon Hogins <jonathanh@unity3d.com>
2020-05-15 15:35:52 -0700

    Attempt at a combined labelingconfiguration. Overly complex I think.

---
+++ com.unity.perception/Editor/GroundTruth/LabelingConfigurationImporter.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Newtonsoft.Json.Linq;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+using UnityEngine.Perception.GroundTruth;
+
+namespace UnityEditor.Perception.GroundTruth
+{
+    [ScriptedImporter(1, "labelconfig")]
+    public class LabelingConfigurationImporter : ScriptedImporter
+    {
+        public override void OnImportAsset(AssetImportContext ctx)
+        {
+            var text = File.ReadAllText(ctx.assetPath);
+            var jObject = JObject.Parse(text);
+            var config = ScriptableObject.CreateInstance<LabelingConfiguration>();
+            if (jObject["auto-assign-ids"].HasValues)
+                config.AutoAssignIds =  jObject["auto-assign-ids"].Value<bool>();
+
+            if (jObject["starting-index"].HasValues)
+            {
+                var startingLabelId = (StartingLabelId)jObject["starting-index"].Value<int>();
+                config.StartingLabelId = startingLabelId;
+            }
+
+            if (jObject["configs"].HasValues)
+            {
+
+            }
+            ctx.SetMainObject(config);
+        }
+    }
+}
