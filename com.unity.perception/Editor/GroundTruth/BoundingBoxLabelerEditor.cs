ee97bafb80c4eb46e7a878792739b9c7aab1d793
Jon Hogins <jonathanh@unity3d.com>
2020-05-19 13:03:16 -0700

    Adding BoundingBoxLabelerEditor.cs

---
+++ com.unity.perception/Editor/GroundTruth/BoundingBoxLabelerEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Perception.GroundTruth;
+
+namespace UnityEditor.Perception.GroundTruth
+{
+    [CustomEditor(typeof(BoundingBoxLabeler))]
+    public class BoundingBoxLabelerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            // base.OnInspectorGUI();
+            // return;
+            EditorGUILayout.PropertyField(this.serializedObject.FindProperty(nameof(BoundingBoxLabeler.annotationId)));
+            var serializedProperty = this.serializedObject.FindProperty(nameof(BoundingBoxLabeler.labelingConfiguration));
+
+            if (serializedProperty.objectReferenceValue != null)
+            {
+                EditorGUILayout.Separator();
+                var editor = Editor.CreateEditor(serializedProperty.objectReferenceValue);
+                editor.OnInspectorGUI();
+            }
+        }
+    }
+}
